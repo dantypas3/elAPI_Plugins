2826d71411ef0f90eb6835ff4d73f096c37f875c
dantypas2 <dantypas2@gmail.com>
2025-05-23 14:36:17 +0200

    add function create_resource

--- plugins/resources/create_resources.py
+++ plugins/resources/create_resources.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 from typing import Union
+from elapi.api import FixedEndpoint
+from utils import resource_utils
+import patch_resources
+import pandas as pd
 
 """
 
@@ -9,5 +13,30 @@ Status: Work in progress
 
 """
 
-def create_ressource_from_json(input_json: Union[Path, str]):
-    pass
+def create_resource():
+    session = FixedEndpoint("items_types")
+    categories = session.get().json()
+    df_categories = pd.json_normalize(categories)
+
+    print("ID  Title")
+    for category in categories:
+        print(f"{category["id"]}: {category['title']}")
+
+    answer = int(input("Please enter id of the resource category to be used: "))
+    category_df = df_categories[df_categories["id"] == answer]
+    print(category_df)
+
+    RESOURCE_CATEGORY_ID = int(category_df["id"])
+    new_resource = resource_utils.FixedResourceEndpoint()
+
+    new_resource.post(
+        data = {"category_id": RESOURCE_CATEGORY_ID}
+    )
+
+    patch_resources.patch_single_resource_from_csv(9948, "test_datei.csv", encoding="utf-8", separator=";")
+
+def main():
+    create_resource()
+
+if __name__ == "__main__":
+    main()
