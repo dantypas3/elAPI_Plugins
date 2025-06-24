2586fde99043f07cee2d3acdc280bdd37d450b10
dantypas2 <dantypas2@gmail.com>
2025-06-24 13:50:17 +0200

    fix bug

--- run_export.bat
+++ run_export.bat
@@ -1,47 +1,42 @@
 @echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
+setlocal
 
-REM
-CD /D "%~dp0"
+cd /d "%~dp0"
 
-REM
 py -3 --version >NUL 2>&1
-IF %ERRORLEVEL% EQU 0 (
-    SET "PY=py -3"
-) ELSE (
+if %ERRORLEVEL% EQU 0 (
+    set "PY=py -3"
+) else (
     python --version >NUL 2>&1
-    IF %ERRORLEVEL% EQU 0 (
-        SET "PY=python"
-    ) ELSE (
-        ECHO No suitable Python interpreter found (tried py -3 and python).
-        PAUSE
-        EXIT /B 1
+    if %ERRORLEVEL% EQU 0 (
+        set "PY=python"
+    ) else (
+        echo No suitable Python. interpreter found (tried py -3 and python)
+        pause
+        exit /b 1
     )
 )
 
-REM
-IF NOT EXIST "venv\Scripts\activate.bat" (
-    ECHO Creating virtualenv…
+if not exist "venv\Scripts\activate.bat" (
+    echo Creating virtualenv
     %PY% -m venv venv
-    IF %ERRORLEVEL% NEQ 0 (
-        ECHO Failed to create virtualenv!
-        PAUSE
-        EXIT /B 1
+    if %ERRORLEVEL% NEQ 0 (
+        echo Failed to create virtualenv!
+        pause
+        exit /b 1
     )
 )
 
-REM
-ECHO Activating virtualenv…
-CALL venv\Scripts\activate.bat
+echo Activating virtualenv
+call venv\Scripts\activate.bat
 
-REM
-IF EXIST requirements.txt (
-    ECHO Installing dependencies…
+if exist requirements.txt (
+    echo Installing dependencies
     pip install --upgrade pip
     pip install -r requirements.txt
 )
 
-REM
+echo Running export_gui
 python -m plugins.resources.export_gui
 
-PAUSE
+pause
